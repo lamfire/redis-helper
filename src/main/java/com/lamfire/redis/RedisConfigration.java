4692fff642ee4102c5e4d67f2ec50203e226f08c
lamfire <52089489@qq.com>
2015-09-15 11:44:28 +0800

    删除内置jedis

--- src/main/java/com/lamfire/redis/RedisConfigration.java
+++ src/main/java/com/lamfire/redis/RedisConfigration.java
@@ -20,15 +20,15 @@ public class RedisConfigration {
         conf = PropertiesUtils.loadAsMap(ClassLoaderUtils.getResourceAsStream(resourceName, RedisConfigration.class));
         logger.info("[RedisConfig]:" + conf);
         init();
-        logger.info("[maxActive]:" +jedisPoolConfig.getMaxActive());
+        logger.info("[redis.pool.maxTotal]:" +jedisPoolConfig.getMaxTotal());
         logger.info("[maxIdle]:" +jedisPoolConfig.getMaxIdle());
-        logger.info("[maxWait]:" +jedisPoolConfig.getMaxWait());
+        logger.info("[maxWait]:" +jedisPoolConfig.getMaxWaitMillis());
         logger.info("[minEvictableIdleTimeMillis]:" +jedisPoolConfig.getMinEvictableIdleTimeMillis());
         logger.info("[minIdle]:" +jedisPoolConfig.getMinIdle());
         logger.info("[numTestsPerEvictionRun]:" +jedisPoolConfig.getNumTestsPerEvictionRun());
         logger.info("[softMinEvictableIdleTimeMillis]:" +jedisPoolConfig.getSoftMinEvictableIdleTimeMillis());
         logger.info("[timeBetweenEvictionRunsMillis]:" +jedisPoolConfig.getTimeBetweenEvictionRunsMillis());
-        logger.info("[whenExhaustedAction]:" +jedisPoolConfig.getWhenExhaustedAction());
+        logger.info("[blockWhenExhausted]:" +jedisPoolConfig.getBlockWhenExhausted());
     }
     
     public JedisPoolConfig getJedisPoolConfig(){
@@ -36,15 +36,15 @@ public class RedisConfigration {
     }
     
     private void init(){
-        jedisPoolConfig.setMaxActive(getMaxActive());
+        jedisPoolConfig.setMaxTotal(getMaxTotal());
         jedisPoolConfig.setMaxIdle(getMaxIdle());
-        jedisPoolConfig.setMaxWait(getMaxWait());
+        jedisPoolConfig.setMaxWaitMillis(getMaxWait());
         jedisPoolConfig.setMinEvictableIdleTimeMillis(getMinEvictableIdleTimeMillis());
         jedisPoolConfig.setMinIdle(getMinIdle());
         jedisPoolConfig.setNumTestsPerEvictionRun(getNumTestsPerEvictionRun());
         jedisPoolConfig.setSoftMinEvictableIdleTimeMillis(getSoftMinEvictableIdleTimeMillis());
         jedisPoolConfig.setTimeBetweenEvictionRunsMillis(getTimeBetweenEvictionRunsMillis());
-        jedisPoolConfig.setWhenExhaustedAction(getWhenExhaustedAction());
+        jedisPoolConfig.setBlockWhenExhausted(getBlockWhenExhausted());
     }
     
     public String getServerHost(){
@@ -56,68 +56,68 @@ public class RedisConfigration {
         return Integer.parseInt(v);
     }
     
-    public int getMaxActive(){
-        String v = conf.get("redis.maxActive");
+    public int getMaxTotal(){
+        String v = conf.get("redis.pool.maxTotal");
         if("".equals(v) || v == null){
             return 16;
         }
         return Integer.parseInt(v);
     }
      public int getMaxIdle(){
-         String v = conf.get("redis.maxIdle");
+         String v = conf.get("redis.pool.maxIdle");
          if("".equals(v) || v == null){
              return 16;
          }
          return Integer.parseInt(v);
      }
      public int getMaxWait(){
-         String v = conf.get("redis.maxWait");
+         String v = conf.get("redis.pool.maxWait");
          if("".equals(v) || v == null){
              return 60000;
          }
          return Integer.parseInt(v);
      }
      public long getMinEvictableIdleTimeMillis(){
-         String v = conf.get("redis.minEvictableIdleTimeMillis");
+         String v = conf.get("redis.pool.minEvictableIdleTimeMillis");
          if("".equals(v) || v == null){
              return 60000;
          }
          return Long.parseLong(v);
      }
      public int getMinIdle(){
-         String v = conf.get("redis.minIdle");
+         String v = conf.get("redis.pool.minIdle");
          if("".equals(v) || v == null){
              return 1;
          }
          return Integer.parseInt(v);
      }
      public int getNumTestsPerEvictionRun(){
-         String v = conf.get("redis.numTestsPerEvictionRun");
+         String v = conf.get("redis.pool.numTestsPerEvictionRun");
          if("".equals(v) || v == null){
              return -1;
          }
          return Integer.parseInt(v);
      }
      public long getSoftMinEvictableIdleTimeMillis(){
-         String v = conf.get("redis.softMinEvictableIdleTimeMillis");
+         String v = conf.get("redis.pool.softMinEvictableIdleTimeMillis");
          if("".equals(v) || v == null){
              return -1;
          }
          return Long.parseLong(v);
      }
      public long getTimeBetweenEvictionRunsMillis(){
-         String v = conf.get("redis.timeBetweenEvictionRunsMillis");
+         String v = conf.get("redis.pool.timeBetweenEvictionRunsMillis");
          if("".equals(v) || v == null){
              return 30000;
          }
          return Long.parseLong(v);
      }
-     public byte getWhenExhaustedAction(){
-         String v = conf.get("redis.whenExhaustedAction");
+     public boolean getBlockWhenExhausted(){
+         String v = conf.get("redis.pool.blockWhenExhausted");
          if("".equals(v) || v == null){
-             return 1;
+             return true;
          }
-         return Byte.parseByte(v);
+         return Boolean.parseBoolean(v);
      }
 
     public int getTimeout(){
